b1eb532d765a53f34cd29d4455acda08dea6ca97
Joschua Thomas Simon-Liedtke (win-10) <joschua@nr.no>
2023-01-16 11:15:54 +0100

    Created package for organize-repo for better organization.

---
+++ code/organize_repo/__init__.py
@@ -0,0 +1,19 @@
+'''
+  Created on 15. jan 2023.
+  
+  Last updated on 15. jan. 2023.
+  
+  @author. joschua
+  
+  This package is used to organize repositories on GitLab and GitHub.
+  
+  Important functions are:
+  * Updating navigation:
+     * In README.md and ARKIV.md.
+     * In weekly Overview.
+     * In intra-weekly navigation.
+  * Including images:
+     * Finding and adding images to .md files.
+     * Adding image tables. 
+'''
+
